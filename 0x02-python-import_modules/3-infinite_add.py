a78e10a89985198ea49613c3eaab252044d92a54
Chinexlee <chinexlee20@gmail.com>
2022-12-02 12:23:09 +0000

    first commit

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    argv_count = len(argv)
+    index = 1
+    sums = 0
+    if index < 1:
+        print("0")
+    else:
+        while index <= argv_count:
+            sum += int(sys.argv[index])
+            index += 1
