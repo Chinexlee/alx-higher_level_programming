764950ebaf5721018d4c5f2fe0abd185871ebe11
Chinexlee <chinexlee20@gmail.com>
2023-01-12 03:18:25 +0000

    new

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -19,27 +19,23 @@ class Rectangle:
     @width.setter
     def width(self, value):
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError('width must be an integer')
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError('width must be >= 0')
         self.__width = value
 
     @property
     def height(self):
         """set/get property of the class height"""
-
         return(self.__height)
 
     @height.setter
     def height(self, value):
-        if type(value) is int:
-            self.__height = value
-
-        elif type(value) != int:
-            raise TypeError("width must be an integer")
-
-        elif height < 0:
-            raise ValueError("width must be >= 0")
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__height = value
 
     def area(self):
         """defining a method area"""
