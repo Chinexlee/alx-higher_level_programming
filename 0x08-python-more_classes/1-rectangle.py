d63e952d622f33c95574629c605aeb32f495c7df
Chinexlee <chinexlee20@gmail.com>
2023-01-12 02:49:57 +0000

    new corrections onsetters and getters

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,14 +20,11 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if type(value) is int:
-            self.__width = value
-
-        elif type(value) != int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
-
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -37,11 +34,8 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if type(value) is int:
-            self.__height = value
-
-        elif type(value) != int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
-
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
+        self.__height = value
