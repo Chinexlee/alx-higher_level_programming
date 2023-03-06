4439f66c94e1f2e8dff33c12324a8e9d7392d712
Chinexlee <chinexlee20@gmail.com>
2023-03-06 04:52:18 -0800

    new chnges

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- lists all records of a database
 SELECT score, name FROM second_table
-ORDER BY score[DESC];
+ORDER BY score DESC;
--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -3,4 +3,4 @@
 -- score in descending order
 SELECT score, name FROM second_table
 WHERE name IS NOT 'NULL'
-ORDER BY score[DESC];
+ORDER BY score DESC;
