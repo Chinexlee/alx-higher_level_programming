0e27582853eb7dade7f2334af013b2c04bd92a79
Chinexlee <chinexlee20@gmail.com>
2023-03-10 12:43:59 -0800

    new

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,5 +3,5 @@
 -- if user exists it should not fail
 -- if database exists, it should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT 0N hbtn_0d_2.* T0 user_0d_2@localhost;
--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,4 @@
---i create a tavle with attributes
+-- create a tavle with attributes
 CREATE TABLE IF NOT EXISTS force_name(
 	id INT,
 	name VARCHAR(256) NOT NULL
