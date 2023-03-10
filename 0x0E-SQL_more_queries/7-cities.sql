2d3a55221588ad853dbad3bc8e2959e5dfd63465
Chinexlee <chinexlee20@gmail.com>
2023-03-10 13:03:28 -0800

    new

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,6 @@
 -- create database and table
 CREATE database IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(
+CREATE TABLE IF NOT EXISTS hbtn_od_usa.cities(
 	id INT NOT NULL AUTO-INCREMENT,
 	state_id INT NOT NULL,
 	name VARCHAR(256),
