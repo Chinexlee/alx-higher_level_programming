70d6334e1b51c26336671d7738e07b90f8117b12
Chinexlee <chinexlee20@gmail.com>
2023-03-10 12:55:53 -0800

    new

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,4 @@
--- create database and user
--- user should have select
--- if user exists it should not fail
--- if database exists, it should not fail
+-- script to run msql commands
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT 0N hbtn_0d_2.* T0 user_0d_2@localhost;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost
