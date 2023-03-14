65d41a45165a4fb27d31d30ad677a7dd1f13383d
Chinexlee <chinexlee20@gmail.com>
2023-03-14 07:08:23 +0000

    new

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-const process = require('process');
-let args = process.argv;
-if(isNaN(args[2]) || args[2] === undefined){
-    console.log('Not a number');
-}
-else{
-    console.log('My Number: ' , parseInt(args[2]));
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', parseInt(process.argv[2]));
 }
